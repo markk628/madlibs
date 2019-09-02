9a9e2b8d6b57582315269d8812fd7ad7dd9a98ad
Donghyun <markk628@gmail.com>
2019-09-01 17:59:52 -0700

    first commit

---
+++ madlibs.py
@@ -0,0 +1,19 @@
+#got the madlib from here -> https://www.woojr.com/mad-libs-worksheets/back-to-school-mad-libs/
+adj1 = input("Enter an adjective: ")
+food1 = input("Enter a food: ")
+food2 = input("Enter another food: ")
+verb1 = input("Enter a verb: ")
+noun1 = input("Enter a noun:" )
+verb2 = input("Enter another verb: ")
+food3 = input("Enter another food: ")
+vegetable = input("Enter a vegetable (plural): ")
+fruits = input("Enter a fruit (plural): ")
+food4 = input("Enter another food: ")
+food5 = input("Enter another food: ")
+adj2 = input("Enter another adjective: ")
+noun2 = input("Enter another noun: ")
+furniture = input("Enter a furniture: ")
+emotion = input("Enter an emotion: ")
+verb3 = input("Enter a past tense verb (-ed): ")
+
+print
